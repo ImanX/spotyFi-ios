22ed5e0dcbac749167925bb2a1377f2f384c29dd
imanx <Imannamix@gmail.com>
2019-03-09 21:07:55 +0330

    add stuff

---
+++ SpotyFi/UIBaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  SpotyFi
+//
+//  Created by ImanX on 3/9/19.
+//  Copyright © 2019 ImanX. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
