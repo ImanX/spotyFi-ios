b12297933a1676e64f6a768d4d3bb2958a07e88d
imanx <Imannamix@gmail.com>
2019-03-09 20:20:20 +0330

    init

---
+++ SpotyFi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotyFi
+//
+//  Created by ImanX on 3/9/19.
+//  Copyright © 2019 ImanX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
