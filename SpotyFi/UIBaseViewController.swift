9570f5b1ccc57bbb7794c8c9fae34b11cd620e50
imanx <Imannamix@gmail.com>
2019-03-09 21:13:27 +0330

    add pod

--- SpotyFi/UIBaseViewController.swift
+++ SpotyFi/UIBaseViewController.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import UIKit
-class BaseViewController: UIViewController {
+class UIBaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
